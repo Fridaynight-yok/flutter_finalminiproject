e5a3d54b94ab34125c1bf97594b1cfbe32dfc0a4
Fridaynight-yok <fridaynight4289@gmail.com>
2025-06-20 18:24:04 +0700

    cat page finish

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_finalminiproject/home.dart';
@@ -11,11 +12,56 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final String uid = FirebaseAuth.instance.currentUser!.uid;
+  Map<String, dynamic>? user;
+
+  Future<void> fetchUser() async {
+    try {
+      final DocumentSnapshot<Map<String, dynamic>> doc = await FirebaseFirestore
+          .instance
+          .collection("member")
+          .doc(uid)
+          .get();
+      if (doc.exists) {
+        user = doc.data();
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("something went wrong")));
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("something went wrong")));
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchUser();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("profile"), centerTitle: true),
-      body: Text("data"),
+      body: Column(
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage("${user!['profile_picture']}"),
+              ),
+            ),
+          ),
+          Text("${user!["name"]}"),
+        ],
+      ), //name is key in map. call name of user.
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
@@ -41,6 +87,7 @@ class _ProfileState extends State<Profile> {
             );
           }
           if (value == 0) {
+            //value is the position of icon. Home is positioned at 0.
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => Home()),
